9b1bbdfe1d9642ac8525957b83ac262465ec9463
fcc-user-m2395 <fcc.user.m2395@gmail.com>
2020-04-13 21:37:43 +0530

    used $ for string concatenation

--- es2015-module-exercise/src/FruitComponent.js
+++ es2015-module-exercise/src/FruitComponent.js
@@ -6,11 +6,14 @@ import { choice, remove } from "./helpers";
 class FruitComponent extends Component {
   render() {
     let fruit = choice(fruits);
-    let changeFruits = remove(fruits, fruit);
-    console.log(" I'd like one" + fruit + ', please');
-    console.log('Here you go:' + fruit);
+    let remainingFruits = remove(fruits, fruit);
+    // console.log(" I'd like one" + fruit + ', please');
+    console.log(`I'd like one ${fruit}, please`);
+    // console.log('Here you go:' + fruit);
+    console.log(`Here you go: ${fruit}`);
     console.log('Delicious! May I have another?');
-    console.log("I'm sorry, we're all out. We have " + changeFruits.length + ' left.');
+    // console.log("I'm sorry, we're all out. We have " + changeFruits.length + ' left.');
+    console.log(`I'm sorry, we're all out. We have ${remainingFruits.length} left.`);
     return <h1>Module Exeercise</h1>
   }
 }
