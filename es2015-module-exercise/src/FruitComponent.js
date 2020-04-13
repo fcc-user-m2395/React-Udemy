fb475e735392ce57b155e145f6da40c823612ed9
fcc-user-m2395 <fcc.user.m2395@gmail.com>
2020-04-13 21:32:22 +0530

    formatting fruit component

--- es2015-module-exercise/src/FruitComponent.js
+++ es2015-module-exercise/src/FruitComponent.js
@@ -1,16 +1,16 @@
-import React,{ Component} from 'react';
+import React, { Component } from 'react';
 
 import fruits from './food';
-import { choice,remove } from "./helpers";
+import { choice, remove } from "./helpers";
 
-class FruitComponent extends Component{
-  render(){
+class FruitComponent extends Component {
+  render() {
     let fruit = choice(fruits);
-    let changeFruits = remove(fruits,fruit);
-   console.log(" I'd like one"+fruit+', please');
-   console.log('Here you go:'+fruit);
-   console.log('Delicious! May I have another?');
-   console.log("I'm sorry, we're all out. We have "+changeFruits.length+' left.');     
+    let changeFruits = remove(fruits, fruit);
+    console.log(" I'd like one" + fruit + ', please');
+    console.log('Here you go:' + fruit);
+    console.log('Delicious! May I have another?');
+    console.log("I'm sorry, we're all out. We have " + changeFruits.length + ' left.');
     return <h1>Module Exeercise</h1>
   }
 }
