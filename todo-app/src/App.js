782c643ecd6a6eb7875456d5f39fb9bc69f5113c
fcc-user-m2395 <fcc.user.m2395@gmail.com>
2020-05-01 18:17:50 +0530

    app deployment

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
 import TodoList from "./TodoList";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <TodoList />
-      </div>
+      <HashRouter basename='/'>
+        <div>
+          {/* <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+          </ul>
+          <hr /> */}
+
+          <Route exact path='/' component={Home} />
+          {/* <Route path='/todo' component={Todo} /> */}
+        </div>
+      </HashRouter>
     );
   }
 }
-
+// const Home = () => (
+//   <div>
+//     <h2>Home</h2>
+//   </div>
+// );
+const Home = () => (
+  <div className='App'>
+    <TodoList />
+  </div>
+);
 export default App;
